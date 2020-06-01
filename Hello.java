58dda5093071962562d1718e02bdf2bb26d61408
hong-ju <ghdwncjs@naver.com>
2020-06-01 15:25:32 +0900

    helloName()

--- Hello.java
+++ Hello.java
@@ -4,10 +4,15 @@ public class Hello {
 
 	public static void main(String[] args) {
 		hello();
+		helloName("man");
+		helloName("Sexy guy");
 		
 	}
 	public static void hello() {
 		System.out.println("Hello k");
+	}
+	public static void helloName(String name) {
+		System.out.println("Hello " + name);
 	}
 	
 	
